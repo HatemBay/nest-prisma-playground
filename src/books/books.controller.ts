7ce19c611e654c03d08e5ab00f4bbeb9a90f2d75
hatemBay <bayoudhhatem5@gmail.com>
2023-09-18 16:54:58 +0100

    Add CRUD for book entity

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,34 +1,50 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { BooksService } from './books.service';
-import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
+import { CreateBookDto } from './dto/create-book.dto';
+import { Book } from '@prisma/client';
 
 @Controller('books')
 export class BooksController {
   constructor(private readonly booksService: BooksService) {}
 
   @Post()
-  create(@Body() createBookDto: CreateBookDto) {
-    return this.booksService.create(createBookDto);
+  async create(@Body() createBookDto: CreateBookDto): Promise<Book> {
+    return await this.booksService.create(createBookDto);
   }
 
   @Get()
-  findAll() {
-    return this.booksService.findAll();
+  async findAll() {
+    return await this.booksService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.booksService.findOne(+id);
+  async findOne(@Param('id') id: string): Promise<Book> {
+    return await this.booksService.findOne({ id: +id });
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateBookDto: UpdateBookDto) {
-    return this.booksService.update(+id, updateBookDto);
+  async update(
+    @Param('id') id: string,
+    @Body() updateBookDto: UpdateBookDto,
+  ): Promise<Book> {
+    const params = {
+      where: { id: +id },
+      data: updateBookDto,
+    };
+    return await this.booksService.update(params);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.booksService.remove(+id);
+  async remove(@Param('id') id: string) {
+    return await this.booksService.remove({ id: +id });
   }
 }
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,26 +1,80 @@
-import { Injectable } from '@nestjs/common';
-import { CreateBookDto } from './dto/create-book.dto';
-import { UpdateBookDto } from './dto/update-book.dto';
-
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+import { Book, Prisma } from '@prisma/client';
 @Injectable()
 export class BooksService {
-  create(createBookDto: CreateBookDto) {
-    return 'This action adds a new book';
+  constructor(private prisma: PrismaService) {}
+
+  async create(createBookDto: Prisma.BookCreateInput): Promise<Book> {
+    try {
+      return await this.prisma.book.create({ data: createBookDto });
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        // The .code property can be accessed in a type-safe manner
+        if (error.code === 'P2002') {
+          console.log(
+            'There is a unique constraint violation, a new user cannot be created with this email',
+          );
+        }
+        if (error.code === 'P2025') {
+          throw new NotFoundException('Please provide correct information');
+        }
+      }
+      throw error;
+    }
   }
 
-  findAll() {
-    return `This action returns all books`;
+  async findAll() {
+    return await this.prisma.book.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} book`;
+  async findOne(where: Prisma.BookWhereUniqueInput) {
+    try {
+      return await this.prisma.book.findUniqueOrThrow({
+        where,
+      });
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === 'P2025') {
+          throw new NotFoundException('Book Not found');
+        }
+      }
+      throw error;
+    }
   }
 
-  update(id: number, updateBookDto: UpdateBookDto) {
-    return `This action updates a #${id} book`;
+  async update(params: {
+    where: Prisma.BookWhereUniqueInput;
+    data: Prisma.BookUpdateInput;
+  }) {
+    const { data, where } = params;
+    try {
+      return await this.prisma.book.update({
+        data,
+        where,
+      });
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === 'P2025') {
+          throw new NotFoundException('Please provide correct information');
+        }
+      }
+      throw error;
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} book`;
+  async remove(where: Prisma.BookWhereUniqueInput) {
+    try {
+      return await this.prisma.book.delete({
+        where,
+      });
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === 'P2025') {
+          throw new NotFoundException('Book Not found');
+        }
+      }
+      throw error;
+    }
   }
 }
